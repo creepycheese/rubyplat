56961c0aa5ccda2c1402c45cc07305d52f69a6c6
Evgeniy Burdaev <hate2me@yandex.ru>
2018-04-18 16:18:52 +0300

    fields for check

--- lib/rubyplat/requests/check.rb
+++ lib/rubyplat/requests/check.rb
@@ -4,11 +4,17 @@ module Rubyplat
       def initialize(params = {})
         @sender = params[:sender]
         @receiver = params[:receiver]
+        @operator = params[:operator]
+        @date = (params[:date] || Time.now).to_s.strftime('%Y.%m.%d %H:%M:%S')
+        @session = params[:session]
       end
 
       def body
         ["SD=#{@sender}",
-         "AP=#{@receiver}"].join("\r\n").encode(Encoding::WINDOWS_1251)
+         "AP=#{@receiver}",
+         "OP=#{@operator}",
+         "DATE=#{@date}",
+         "SESSION=#{@session}"].join("\r\n").encode(Encoding::WINDOWS_1251)
       end
     end
   end
