1391cb38582a9aff8182b4517391a6808433e63d
Evgeniy Burdaev <hate2me@yandex.ru>
2018-04-27 17:23:34 +0300

    check request

--- lib/rubyplat/requests/check.rb
+++ lib/rubyplat/requests/check.rb
@@ -1,20 +1,41 @@
 module Rubyplat
   module Requests
     class Check
+      InvalidPaytool = Class.new(StandardError)
+      PAYTOOL = {
+        cash: 0,
+        local_card: 1,
+        foreign_card: 2
+      }
 
       # @param [Hash] params arguments to create request
-      # @option params :sender sender of request
-      # @option params :receiver request receiver
-      # @option params :operator operator
-      # @option params :date request date
-      # @option params :session session
+      # @option [String] params :sender sender of request
+      # @option [String] params :receiver request receiver
+      # @option [String] params :operator operator
+      # @option [String] params :date request date
+      # @option [String] params :session session
+      # @option [String] params :number phone number or payer's account number(can be blank)
+      # @option [String] params :account payer's identifier or payees service identifier. Can be blank.
+      # @option [Float] params :amount amount to be payed. example: 1234.12
+      #
       def initialize(params = {})
+        params = defaults.merge(params)
+
         @sender = params[:sender]
         @receiver = params[:receiver]
         @operator = params[:operator]
         @date = DateTime.parse((params[:date] || Time.now).to_s).strftime('%Y.%m.%d %H:%M:%S')
         @session = params[:session]
         @number = params[:number]
+        @account = params[:account]
+        @amount = params[:amount]
+        @req_type = params[:req_type]
+        @pay_tool = PAYTOOL.fetch(params[:pay_tool]) { raise InvalidPaytool.new("Paytool может принимать значения #{PAYTOOL.keys}") }
+        @term_id = params[:term_id]
+        @comment = params[:comment]
+        @accept_keys = params[:accept_keys]
+        @no_route = params[:no_route]
+        @amount_all = params[:amount_all]
       end
 
       # @return [String] String representation of request
@@ -24,15 +45,46 @@ module Rubyplat
       #     receiver: 'receiver',
       #     operator: 'operator',
       #     date: Time.now,
-      #     session: 'sess')
-      #   @check_request.body #=> "SD=sender\r\nAP=receiver\r\nOP=operator\r\nDATE=2018.04.23 16:47:28\r\nSESSION=sess"
+      #     session: 'sess',
+      #     account: 'account',
+      #     amount: 1234.42,
+      #     req_type: false,
+      #     term_id: '12345',
+      #     comment: 'FooBar',
+      #     accept_keys: '12345',
+      #     pay_tool: :cash,
+      #     no_route: true)
+      #
+      #   @check_request.body
+      #    #=> "SD=sender\r\nAP=receiver\r\nOP=operator\r\nDATE=2018.04.27 17:08:49\r\nSESSION=sess\r\nACCOUNT=account\r\nAMOUNT=1234.42\r\nREQ_TYPE=0\r\nPAY_TOOL=0\r\nTERM_ID=12345\r\nCOMMENT=FooBar\r\nACCEPT_KEYS=12345\r\nNO_ROUTE=1"
       def body
-        ["SD=#{@sender}",
-         "AP=#{@receiver}",
-         "OP=#{@operator}",
-         "DATE=#{@date}",
-         "SESSION=#{@session}",
-         @number && "NUMBER=#{@number}"].compact.join("\r\n").encode(Encoding::WINDOWS_1251)
+        body_parameters.compact.join("\r\n").encode(Encoding::WINDOWS_1251)
+      end
+
+      private
+
+      def defaults
+        { pay_tool: 0, no_route: false }
+      end
+
+      def body_parameters
+        [
+          "SD=#{@sender}",
+          "AP=#{@receiver}",
+          "OP=#{@operator}",
+          "DATE=#{@date}",
+          "SESSION=#{@session}",
+          @number && "NUMBER=#{@number}",
+          "ACCOUNT=#{@account}",
+          "AMOUNT=#{@amount}",
+          "REQ_TYPE=#{@req_type ? 1 : 0}",
+          "PAY_TOOL=#{@pay_tool}",
+          "TERM_ID=#{@term_id}",
+          "COMMENT=#{@comment}",
+          "ACCEPT_KEYS=#{@accept_keys}",
+          "NO_ROUTE=#{@no_route ? 1 : 0}",
+          "AMOUNT_ALL=#{@amount_all}"
+        ]
       end
     end
   end
--- test/requests/check_test.rb
+++ test/requests/check_test.rb
@@ -7,7 +7,16 @@ class Rubyplat::CheckTest < Minitest::Test
       receiver: 'receiver',
       operator: 'operator',
       date: Time.now,
-      session: 'sess')
+      session: 'sess',
+      account: 'account',
+      amount: 1234.42,
+      amount_all: 1234.11,
+      req_type: false,
+      term_id: '12345',
+      comment: 'FooBar',
+      accept_keys: '12345',
+      pay_tool: :cash,
+      no_route: true)
   end
 
   def test_it_renders_sender
@@ -41,8 +50,71 @@ class Rubyplat::CheckTest < Minitest::Test
       operator: 'operator',
       date: Time.now,
       session: 'sess',
-      number: 333)
+      number: 333,
+      pay_tool: :cash)
 
     assert @check_request.body.include?('NUMBER=333')
+  end
+
+  def test_it_renders_account_when_given
+    assert @check_request.body.include?('ACCOUNT=account')
+  end
+
+  def test_it_renders_amount
+    assert @check_request.body.include?('AMOUNT=1234.42')
+  end
+
+  def test_it_renders_req_type_zero_when_false
+    assert @check_request.body.include?('REQ_TYPE=0')
+  end
+
+  def test_it_renders_req_type_to_one_when_true
+    @check_request = Rubyplat::Requests::Check.new(
+      sender: 'sender',
+      receiver: 'receiver',
+      operator: 'operator',
+      date: Time.now,
+      session: 'sess',
+      account: 'account',
+      amount: 1234.42,
+      req_type: true,
+      pay_tool: :cash)
+
+    assert @check_request.body.include?('REQ_TYPE=1')
+  end
+
+  def test_it_renders_paytool_for_local_card
+    @check_request = Rubyplat::Requests::Check.new(
+      sender: 'sender',
+      receiver: 'receiver',
+      operator: 'operator',
+      date: Time.now,
+      session: 'sess',
+      account: 'account',
+      amount: 1234.42,
+      req_type: false,
+      pay_tool: :local_card)
+
+    assert @check_request.body.include?('PAY_TOOL=1')
+  end
+
+  def test_it_renders_term_id
+    assert @check_request.body.include?('TERM_ID=12345')
+  end
+
+  def test_it_renders_cooment
+    assert @check_request.body.include?('COMMENT=FooBar')
+  end
+
+  def test_it_renders_accept_keys
+    assert @check_request.body.include?('ACCEPT_KEYS=12345')
+  end
+
+  def test_it_renders_no_route
+    assert @check_request.body.include?('NO_ROUTE=1')
+  end
+
+  def test_it_redners_amount_all
+    assert @check_request.body.include?('AMOUNT_ALL=1234.11')
   end
 end
